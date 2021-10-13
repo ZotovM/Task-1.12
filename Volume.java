f4deac9ecf0c1b461633db9f341dad97dfe9e51f
ZotovM <90614959+ZotovM@users.noreply.github.com>
2021-10-13 14:40:53 +0300

    Add files via upload

--- Volume.java
+++ Volume.java
@@ -1,17 +1,28 @@
-import java.util.Scanner;
+import java.util.Scanner; //Вычислить объем цилиндра c основанием радиуса R и высотой H.
 import java.util.Locale;
 public class Volume {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
-        double R = readNumber("Введите значение: R ");
+        double R = readNumber("Введите значение R: ");
         double h = readNumber("Введите значение h: ");
-        double V = Math.PI * Math.pow(R, 2) * h;
-        System.out.println("Объем цилиндра равен: " + V);
-        System.exit(0);
+        double V = calc(R, h);
+        answer(V);
     }
-        private static double readNumber(String text) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println(text);
-            return scanner.nextDouble();
+    private static double calc(double R, double h) {
+        double V = Math.pow(R, 2) * Math.PI * h;
+        return V;
+    }
+    private static void answer(double V) {
+        if (V <= 0) {
+            System.out.println("Ошибка! ");
+        }
+        else {
+            System.out.println("Объем равен: " + V);
         }
     }
+    private static double readNumber(String text) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(text);
+        return scanner.nextDouble();
+    }
+}
